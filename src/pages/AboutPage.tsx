376ee793436b484d930b98469152ad4f79f54936
Henrique Honorio da Silva <contato@henriquesilva.dev>
2025-05-08 13:11:11 -0300

    update about

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,7 +7,7 @@ import VerticalSidebar from '../components/VerticalSidebar';
 const AboutPage: React.FC = () => {
   return (
     <div className="bg-[#191919] min-h-screen text-white relative overflow-hidden flex flex-col">
-      <div className="container mx-auto px-4 ml-[250px] md:px-6 py-16 md:py-24 flex-1">
+      <div className="container mx-auto px-4 md:px-6 py-16 md:py-24 flex-1">
         <div className="flex flex-col md:flex-row items-center md:items-start gap-8 md:gap-12 lg:gap-16">
           <ProfilePhoto />
           {/* Texto justificado sempre */}
