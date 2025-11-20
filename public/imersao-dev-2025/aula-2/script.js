6b980b00bd3a535dfb4bd21937d165d021bc1e14
Henrique Honorio da Silva <contato@henriquesilva.dev>
2025-11-20 11:19:32 -0300

    feat: update aula3

--- public/imersao-dev-2025/aula-2/script.js
+++ public/imersao-dev-2025/aula-2/script.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     articleContainer.innerHTML = `
-      <article class="content-item hero hero-layout">
+      <article class="content-item hero hero-layout" id="initial-content">
 
         <div class="hero-copy">
           <a href="https://henriquesilva.dev/imersao-dev-2025/aula-2/" class="hero-kicker">Voltar a Info</a>
